b13e145989706830d8d92cce1d04e55b21d5d0aa
khoahuynh88 <khoahuynh1088@gmail.com>
2023-04-26 14:36:45 -0500

    delete bearer

--- DoConnect-angular/src/app/auth/auth.interceptor.ts
+++ DoConnect-angular/src/app/auth/auth.interceptor.ts
@@ -49,7 +49,7 @@ export class AuthInterceptor implements HttpInterceptor {
       return request.clone(
           {
               setHeaders: {
-                  Authorization : `Bearer ${token}`
+                  Authorization : `${token}`
               }
           }
       );
